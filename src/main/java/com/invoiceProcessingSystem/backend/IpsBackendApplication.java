a137c4d282b5534d9e05c5ace6c7ffdd829774f7
kush-prog <chauhankush12345@gmail.com>
2025-03-01 10:23:44 +0530

    Project structure setted up

---
+++ src/main/java/com/invoiceProcessingSystem/backend/IpsBackendApplication.java
@@ -0,0 +1,13 @@
+package com.invoiceProcessingSystem.backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IpsBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(IpsBackendApplication.class, args);
+	}
+
+}
